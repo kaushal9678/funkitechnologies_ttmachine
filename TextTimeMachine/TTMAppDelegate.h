6840dfa314e34584a5dbfcbb9156863ef83643f2
Komal Verma <komalverma@Komals-Mac-mini.local>
2014-02-28 22:00:36 +0530

    Initial Commit

---
+++ TextTimeMachine/TTMAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  TTMAppDelegate.h
+//  TextTimeMachine
+//
+//  Created by Komal Verma on 28/02/14.
+//  Copyright (c) 2014 Komal Verma. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TTMAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TextTimeMachine/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TextTimeMachine
+//
+//  Created by Komal Verma on 28/02/14.
+//  Copyright (c) 2014 Komal Verma. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "TTMAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([TTMAppDelegate class]));
+    }
+}
